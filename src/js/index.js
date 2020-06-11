e50e588463c5b1a5364c9d730f9ee5c994612c64
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-06-12 01:01:03 +0200

    work in progress..

--- src/js/index.js
+++ src/js/index.js
@@ -18,12 +18,14 @@ sun.addEventListener("click", () => {
 		console.log("the night is dark as the buttom of the coal mine");
 		isDark = true;
 		document.documentElement.style.setProperty("--sky", "#090809");
+		document.documentElement.style.setProperty("--text-color", "#F1FFE7");
+		document.documentElement.style.setProperty("--sun", "../assets/img/moon.png");
 		createContent(".sun__text--js", "the night is dark");
-    document.documentElement.style.setProperty(" --text-color, #F1FFE7")
 	} else {
 		console.log("just another bright day");
 		isDark = false;
 		document.documentElement.style.setProperty("--sky", "#1AC8ED");
+		document.documentElement.style.setProperty("--text-color", "#090809");
 		createContent(".sun__text--js", "such a lovely day");
 	}
 });
