99db518da38a2cf0f394158c63db62ed781fb0cb
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-06-11 20:02:19 +0200

    eventlistener on sun added

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
 
+let isDark = false;
 
+document.documentElement.style.setProperty("--sky", "#090809");
 
-document.documentElement.style.setProperty('--sky', '#090809');
+const sun = document.querySelector(".sun--js");
+
+sun.addEventListener("click", () => {
+	if (isDark == false) {
+		console.log("it is bright day");
+	} else {
+		console.log("this night is dark as your soul");
+	}
+});
